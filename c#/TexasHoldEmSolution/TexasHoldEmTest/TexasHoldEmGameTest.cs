68d5c9007d1e72b69848934b9bbae37e10c0c4e5
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-03-03 22:10:58 +0100

    add empty texas hold'em game class

---
+++ c#/TexasHoldEmSolution/TexasHoldEmTest/TexasHoldEmGameTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TexasHoldEmTest
+{
+    [TestClass]
+    public class TexasHoldEmGameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
