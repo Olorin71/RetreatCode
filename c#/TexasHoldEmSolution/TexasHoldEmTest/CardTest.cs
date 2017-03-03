32b4add65895780cbaff39c97d1b7f4d7a2167db
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-03-03 17:52:55 +0100

    Add some Card Test and exclude test classes from VS code coverage

---
+++ c#/TexasHoldEmSolution/TexasHoldEmTest/CardTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldEm;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TexasHoldEmTest
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class CardTest
+    {
+        [TestMethod]
+        public void CanCreateCard()
+        {
+            Card card = new Card(CardValue.Four, CardSuit.Diamond);
+            Assert.IsNotNull(card);
+        }
+        [TestMethod]
+        public void CardFourDiamondAsStringReturnFourOfDiamonds()
+        {
+            Card card = new Card(CardValue.Four, CardSuit.Diamond);
+            Assert.AreEqual("Four of Diamonds", card.ToString());
+        }
+    }
+}
--- c#/TexasHoldEmSolution/TexasHoldEmTest/DeckTest.cs
+++ c#/TexasHoldEmSolution/TexasHoldEmTest/DeckTest.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
 using TexasHoldEm;
 
 namespace TexasHoldEmTest
 {
     [TestClass]
+    [ExcludeFromCodeCoverage]
     public class DeckTest
     {
         [TestMethod]
