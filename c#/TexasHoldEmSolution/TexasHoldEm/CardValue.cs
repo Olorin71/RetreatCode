ce111480fc0b285962f6b9cd1fbf48854baae6c6
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-03-03 17:54:36 +0100

    Rename Card Values

--- c#/TexasHoldEmSolution/TexasHoldEm/CardValue.cs
+++ c#/TexasHoldEmSolution/TexasHoldEm/CardValue.cs
@@ -2,7 +2,7 @@
 {
     public enum CardValue
     {
-        A = 14, K = 13, Q = 12, J = 11, Ten = 10, Nine = 9, Eight = 8, Seven = 7, Six = 6, Five = 5, Four = 4, Three = 3, Two = 2
+        Ace = 14, King = 13, Queen = 12, Jack = 11, Ten = 10, Nine = 9, Eight = 8, Seven = 7, Six = 6, Five = 5, Four = 4, Three = 3, Two = 2
     }
 
 }
