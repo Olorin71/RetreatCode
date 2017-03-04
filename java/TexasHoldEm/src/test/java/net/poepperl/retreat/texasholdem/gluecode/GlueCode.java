e699c66d53048f5ee58e1c1cf0ad6c788f621c36
Nils Pöpperl <nils@poepperl.net>
2017-03-04 22:38:50 +0100

    adds a basic structure for cucumber tests

---
+++ java/TexasHoldEm/src/test/java/net/poepperl/retreat/texasholdem/gluecode/GlueCode.java
@@ -0,0 +1,20 @@
+package net.poepperl.retreat.texasholdem.gluecode;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GlueCode {
+
+	@Given("^Gegeben sei$")
+	public void gegeben_sei() throws Throwable {
+	}
+
+	@When("^Ein Ereignis, dass eintritt \\(oder Befehl der aufgerufen wird\\)$")
+	public void ein_Ereignis_dass_eintritt_oder_Befehl_der_aufgerufen_wird() throws Throwable {
+	}
+
+	@Then("^Dann wird ein bestimmtes Ergebnis erwartet$")
+	public void dann_wird_ein_bestimmtes_Ergebnis_erwartet() throws Throwable {
+	}
+}
---
+++ java/TexasHoldEm/src/test/resources/features/dummy.feature
@@ -0,0 +1,7 @@
+Feature: Name des Features
+	Eine Beschreibung des Features z.B. als UserStory
+
+	Scenario: Ein konkretes Szenario
+		Given Gegeben sei
+		When Ein Ereignis, dass eintritt (oder Befehl der aufgerufen wird)
+		Then Dann wird ein bestimmtes Ergebnis erwartet
