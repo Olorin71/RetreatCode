53fea608585562f7dc61c7d072191bfe5ea8d885
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-03-05 14:05:00 +0100

    extract base class for straight family

---
+++ c#/TexasHoldEmSolution/TexasHoldEm/Internals/StraightCheckerBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TexasHoldEm.Interfaces;
+
+namespace TexasHoldEm.Internals
+{
+    internal abstract class StraightCheckerBase : CheckerBase
+    {
+        protected StraightCheckerBase(CheckerData data, ComparerHelper comparer) : base(data, comparer)
+        {
+        }
+
+        protected int MinValue { get; set; }
+
+        protected override IList<ICard> GetCards()
+        {
+            List<ICard> cards = new List<ICard>();
+            var ace = Data.Cards.First(x => x.Value == (MinValue == 1 ? CardValue.Ace : (CardValue)MinValue));
+            cards.Add(ace);
+            var two = Data.Cards.First(x => x.Value == (CardValue)(MinValue + 1));
+            cards.Add(two);
+            var three = Data.Cards.First(x => x.Value == (CardValue)(MinValue + 2));
+            cards.Add(three);
+            var four = Data.Cards.First(x => x.Value == (CardValue)(MinValue + 3));
+            cards.Add(four);
+            var five = Data.Cards.First(x => x.Value == (CardValue)(MinValue + 4));
+            cards.Add(five);
+            return cards;
+        }
+
+
+        protected CardValue CalculateKey(int lowerPosition, int position)
+        {
+            var value = (lowerPosition + position - 1);
+            return value == 1 ? CardValue.Ace : (CardValue)value;
+        }
+
+    }
+}
--- c#/TexasHoldEmSolution/TexasHoldEm/Internals/StraightFlushChecker.cs
+++ c#/TexasHoldEmSolution/TexasHoldEm/Internals/StraightFlushChecker.cs
@@ -4,30 +4,13 @@ using TexasHoldEm.Interfaces;
 
 namespace TexasHoldEm.Internals
 {
-    internal class RoyalAndStraightFlushChecker : CheckerBase
+    internal class RoyalAndStraightFlushChecker : StraightCheckerBase
     {
         public RoyalAndStraightFlushChecker(CheckerData data, ComparerHelper comparer) : base(data, comparer)
         {
         }
 
-        private int minValue;
         private List<ICard> cards = new List<ICard>();
-
-        protected override IList<ICard> GetCards()
-        {
-            List<ICard> cards = new List<ICard>();
-            var ace = Data.Cards.First(x => x.Value == (minValue == 1 ? CardValue.Ace : (CardValue)minValue));
-            cards.Add(ace);
-            var two = Data.Cards.First(x => x.Value == (CardValue)(minValue + 1));
-            cards.Add(two);
-            var three = Data.Cards.First(x => x.Value == (CardValue)(minValue + 2));
-            cards.Add(three);
-            var four = Data.Cards.First(x => x.Value == (CardValue)(minValue + 3));
-            cards.Add(four);
-            var five = Data.Cards.First(x => x.Value == (CardValue)(minValue + 4));
-            cards.Add(five);
-            return cards;
-        }
 
         protected override bool HasHand()
         {
@@ -51,7 +34,7 @@ namespace TexasHoldEm.Internals
                         }
                         if (position == 6)
                         {
-                            minValue = lowerPosition;
+                            MinValue = lowerPosition;
                             HandName = lowerPosition == 10 ? HandName.RoyalFlush : HandName.StraightFlush;
                             straightFound = true;
                         }
@@ -62,10 +45,5 @@ namespace TexasHoldEm.Internals
             return straightFound;
         }
 
-        private CardValue CalculateKey(int lowerPosition, int position)
-        {
-            var value = (lowerPosition + position - 1);
-            return value == 1 ? CardValue.Ace : (CardValue)value;
-        }
     }
 }
