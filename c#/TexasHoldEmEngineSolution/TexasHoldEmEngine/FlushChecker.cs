aa1dd5c46187b502d92a880de60785c14d863793
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-03-07 19:03:06 +0100

    there can be only one suit with at least five cards. So use First() not Where().

--- c#/TexasHoldEmEngineSolution/TexasHoldEmEngine/FlushChecker.cs
+++ c#/TexasHoldEmEngineSolution/TexasHoldEmEngine/FlushChecker.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using TexasHoldEmEngine.Interfaces;
 
@@ -14,19 +13,27 @@ namespace TexasHoldEmEngine
 
         protected override IList<ICard> GetCards()
         {
-            var suit = Data.CardSuitsDistribution.Where(x => x.Value >= 5);
-            var allSuitedCards = Data.Cards.Where(x => x.Suit == suit.First().Key).Select(x => x.Value).ToList();
+            var suit = Data.CardSuitsDistribution.First(x => x.Value >= 5).Key;
+            var allSuitedCards = Data.Cards.Where(x => x.Suit == suit).Select(x => x.Value).ToList();
+            IList<ICard> cards = GetTheFiveHigherCards(suit, allSuitedCards);
+            return cards;
+        }
+
+        private IList<ICard> GetTheFiveHigherCards(CardSuit suit, List<CardValue> allSuitedCards)
+        {
             SortByCardValue(allSuitedCards);
             IList<ICard> cards = new List<ICard>();
             for (var index = 0; index < 5; index++)
             {
-                cards.Add(Data.Cards.First(x => x.Value == allSuitedCards[index] && x.Suit == suit.First().Key));
+                cards.Add(Data.Cards.First(x => x.Value == allSuitedCards[index] && x.Suit == suit));
             }
+
             return cards;
         }
 
         protected override bool HasHand()
         {
+            // True if there is one suit with at least five cards
             return Data.CardSuitsDistribution.Any(x => x.Value >= 5);
 
         }
