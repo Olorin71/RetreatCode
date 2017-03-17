e3d09614acc68f1e1a6a0ff7d13f216a1b2b4cb6
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2017-03-17 15:20:30 +0100

    correct hand merge error

--- c#/TexasHoldEmEngineSolution/TexasHoldEmEngine.Interfaces/IHandComparer.cs
+++ c#/TexasHoldEmEngineSolution/TexasHoldEmEngine.Interfaces/IHandComparer.cs
@@ -6,6 +6,6 @@ namespace TexasHoldEmEngine.Interfaces
     public interface IHandComparer
     {
         // ToDo: Wäre eine Liste von IPlayer zurück besser?
-        IList<Guid> FindRoundWinners(IDictionary<Guid, IPlayer> players, IList<ICard> communityCards);
+        IList<Guid> FindRoundWinners(IDictionary<Guid, IPlayerHoleCards> players, IList<ICard> communityCards);
     }
 }
